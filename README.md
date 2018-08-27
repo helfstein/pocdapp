c9e58dbb26495a0ddb504275d0be32c071e62949
Helfstein <Helfstein-Note>
2018-08-27 13:47:31 -0300

    readme edited

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # PocDApp
 ## to run this project follow the steps below
-We will need to instal dependencies for truffle template, angular and etherum-bridge
+First we'll need to instal dependencies for truffle template, angular and etherum-bridge
 
 ## 1 - Install dependencies for etherum-bridge
+* $ cd etherum-bridge
+* $ npm install
 
 ## 2 - Install dependencies for truffle template 
+* $ cd ..
 * $ cd src
-* $ npm i
+* $ npm install
 
 ## 3 - Install dependencies for angular project
 * $ cd dapp
-* $ npm i
+* $ npm install
 
-##
+## 4 - Edit truffle.js file located to point to etherum node like Ganache geth or truffle develop. The code sample is below.
+module.exports = {
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas:500000
+      
+    },
+  }
+}
+
---
+++ src/contracts/PocDapp.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.24;
+
+import "installed_contracts/oraclize-api/contracts/usingOraclize.sol";
+import "../../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract PocDapp is usingOraclize, Ownable {
+    
+    uint articleRegisryFee = 0.00001 ether;  
+        
+    event CallbackReceived(string result);
+    event ValueSended(uint value);
+
+    constructor() public payable {
+        OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
+
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+    }
+
+    // Fallback function
+    function() public{
+        revert("reverted");
+    }
+
+    function __callback(bytes32 id, string result, bytes proof) public {
+        require(msg.sender == oraclize_cbAddress(), "This address is not a valid coinbase");
+
+        emit CallbackReceived(result);        
+    }
+
+    function refreshEthPrice() public payable {
+        uint estimatedTransactionPrice = oraclize_getPrice("URL") + articleRegisryFee;
+        emit ValueSended(msg.value);
+        address(this).balance + msg.value;
+        require(estimatedTransactionPrice < msg.value, "Not enough gas to execute this method");
+        //address(this).balance + articleRegisryFee;
+        oraclize_query("URL", "json(https://api.coinbase.com/v2/prices/ETH-USD/spot).data.amount");
+    }
+ 
+    function getOraclizeFee() public returns (uint _fee) {
+        return oraclize_getPrice("URL");
+    }
+
+    function getFee() public returns (uint _fee) {
+        return articleRegisryFee;
+    }
+
+    function getBalance() public returns (uint _balance) {
+        return address(this).balance;
+    }
+
+}
